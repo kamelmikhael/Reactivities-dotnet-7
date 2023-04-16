cfb93ea09a3beb664013a74a2f0fd59376e0c309
kameldanial <kamelmikhaelkamel@gmail.com>
2023-04-16 10:24:11 +0200

    update validations

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -9,22 +9,16 @@ namespace API.Controllers
     public class ActivitiesController : BaseApiController
     {
         [HttpGet]
-        public async Task<IActionResult> GetActivities([FromQuery] ActivityParams param)
-        {
-            return HandlePagedResult(await Mediator.Send(new List.Query { Params = param }));
-        }
+        public async Task<IActionResult> GetActivities([FromQuery] ActivityParams param) => 
+            HandlePagedResult(await Mediator.Send(new List.Query { Params = param }));
 
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetActivity(Guid id)
-        {
-            return HandleResult(await Mediator.Send(new Details.Query { Id = id }));
-        }
+        public async Task<IActionResult> GetActivity(Guid id) =>
+            HandleResult(await Mediator.Send(new Details.Query { Id = id }));
 
         [HttpPost]
-        public async Task<IActionResult> CreateActivity(Activity activity)
-        {
-            return HandleResult(await Mediator.Send(new Create.Command { Activity = activity }));
-        }
+        public async Task<IActionResult> CreateActivity(Activity activity) => 
+            HandleResult(await Mediator.Send(new Create.Command { Activity = activity }));
 
         [Authorize(Policy = "IsActivityHost")]
         [HttpPut("{id}")]
@@ -36,15 +30,11 @@ namespace API.Controllers
 
         [Authorize(Policy = "IsActivityHost")]
         [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(Guid id)
-        {
-            return HandleResult(await Mediator.Send(new Delete.Command { Id = id }));
-        }
+        public async Task<IActionResult> Delete(Guid id) => 
+            HandleResult(await Mediator.Send(new Delete.Command { Id = id }));
 
         [HttpPost("{id}/attend")]
-        public async Task<IActionResult> Attend(Guid id)
-        {
-            return HandleResult(await Mediator.Send(new UpdateAttendance.Command { Id = id }));
-        }
+        public async Task<IActionResult> Attend(Guid id) =>
+            HandleResult(await Mediator.Send(new UpdateAttendance.Command { Id = id }));
     }
 }
--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -35,10 +35,13 @@ axios.interceptors.response.use(async response => {
     const { data, status, config } = error.response as AxiosResponse;
     switch (status) {
         case 400:
-            if (config.method === 'get' && data.errors.hasOwnProperty('id')) {
+            if(typeof data === 'string') {
+                toast.error(data);
+            }
+            else if (config.method === 'get' && data.errors.hasOwnProperty('id')) {
                 router.navigate('/not-found');
             }
-            if (data.errors) {
+            else if (data.errors) {
                 const modalStateErrors = [];
                 for (const key in data.errors) {
                     if (data.errors[key]) {
@@ -46,8 +49,6 @@ axios.interceptors.response.use(async response => {
                     }
                 }
                 throw modalStateErrors.flat();
-            } else {
-                toast.error(data);
             }
             break;
         case 401:
